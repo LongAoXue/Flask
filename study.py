52dae15be75233104630bc295e985c5b06ca2767
main <wdwindows77@outlook.com>
2026-04-27 13:08:23 +0800

    .

--- study.py
+++ study.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('tmp4.html')
+    nick='少羽'
+    return render_template('tmp4.html', nick=nick)
 
 if __name__ == '__main__':
     app.run(debug=True)
